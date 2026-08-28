ab7de649d73ea667ff2ef7a11a1157381a0678b5
Eugenia <eugeniacoarasa@gmail.com>
2026-08-28 16:24:39 -0300

    fix: actualiza version de cache en sw.js a v1.2.0

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'trivia-school-v1';
+const CACHE_NAME = 'trivia-school-v1.2.0';
 const urlsToCache = [
   './index.html',
   './script.js',
-  './preguntas.js',
+  './preguntas_3.js',
+  './preguntas_4.js',
+  './preguntas_5.js',
+  './preguntas_6.js',
   './manifest.json'
 ];
 
